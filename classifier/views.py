247e5505150f424159a68f65f49a4e0a4c50aa2d
Soham Gadhave <soham.gadhave@somaiya.edu>
2021-03-06 21:01:18 +0530

    change response and handle exceptions with status code

--- classifier/views.py
+++ classifier/views.py
@@ -21,50 +21,74 @@ def index(request):
 
 	if request.method == 'GET':
 		
-		data = json.loads(request.body.decode("utf-8"))
-		
-		x = clean_and_extract([str(data['question_text'])])
-		x_te = x[0]
-		x_te1 = x[1]
-		x_c = x[2]
-
-		prob_LR_tfidf = LR_tfidf.predict_proba(x_te)[:,1][0]
-		prob_SVC_tfidf = SVC_tfidf._predict_proba_lr(x_te)[:,1][0]
-		prob_NB_count = NB_count.predict_proba(x_c)[:,1][0]
-		prob_LR_count = LR_count.predict_proba(x_te1)[:,1][0]
-		prob_SVC_count = SVC_count._predict_proba_lr(x_te1)[:,1][0]
-
-
-		Type = []
-		probabilities = [prob_LR_tfidf, prob_SVC_tfidf, prob_NB_count, prob_LR_count, prob_SVC_count]
-		
-		for prob in probabilities:
-			if prob >= 0.5:
-				Type.append("Insincere")
+		# print(request.GET.get("question_text"))
+		try:
+			if request.GET.get("question_text"):
+				data = {'question_text': request.GET.get("question_text")}
 			else:
-				Type.append("Sincere")
+				data = json.loads(request.body.decode("utf-8"))
+		except (TypeError, json.JSONDecodeError):
+			return JsonResponse({"message": "Bad Request, check if you are not sending Empty or invalid Data"}, status=400)
 
-		response_body = {
-			"question_text" : str(data['question_text']),
-			"type_LR_tfidf" : Type[0],
-			"probability_LR_tfidf" : probabilities[0],
-			"type_SVC_tfidf" : Type[1],
-			"probability_SVC_tfidf" : probabilities[1],
-			"type_NB_tfidf" : Type[2],
-			"probability_NB_count" : probabilities[2],
-			"type_LR_count" : Type[3],
-			"probability_LR_count" : probabilities[3],
-			"type_SVC_count" : Type[4],
-			"probability_SVC_count" : probabilities[4]
-		}
+		try:
+			x = clean_and_extract([str(data['question_text'])])
+			x_te = x[0]
+			x_te1 = x[1]
+			x_c = x[2]
 
-		response = JsonResponse(response_body)
-		
-		return response
+			prob_LR_tfidf = LR_tfidf.predict_proba(x_te)[:,1][0]
+			prob_LR_count = LR_count.predict_proba(x_te1)[:,1][0]
+			prob_SVC_tfidf = SVC_tfidf._predict_proba_lr(x_te)[:,1][0]
+			prob_SVC_count = SVC_count._predict_proba_lr(x_te1)[:,1][0]
+			prob_NB_count = NB_count.predict_proba(x_c)[:,1][0]
+
+
+			Type = []
+			probabilities = [prob_LR_tfidf, prob_LR_count, prob_SVC_tfidf, prob_SVC_count, prob_NB_count]
+			
+			for prob in probabilities:
+				if prob >= 0.5:
+					Type.append("Insincere")
+				else:
+					Type.append("Sincere")
+
+			response_body = {
+				"question_text" : str(data['question_text']),
+				"models": {
+					"LR_TFIDF": {
+						"type": Type[0],
+						"probability": probabilities[0] 
+					},
+					"LF_COUNT": {
+						"type": Type[1],
+						"probability": probabilities[1] 
+					},
+					"SVC_TFIDF": {
+						"type": Type[2],
+						"probability": probabilities[2] 
+					},
+					"SVC_COUNT": {
+						"type": Type[3],
+						"probability": probabilities[3] 
+					},
+					"NB": {
+						"type": Type[4],
+						"probability": probabilities[4] 
+					}
+				}
+			}
+
+			response = JsonResponse(response_body, status=200)
+			
+			return response
+		except Exception as exception:
+			return JsonResponse({"message" : "Sorry something went wrong, counld not process ypur request. Try again", "error": exception}, status=500)
 
 	if request.method == "POST":
 
 		data = json.loads(request.body.decode("utf-8"))
+
+		print(data)
 
 		questions = []
 		for ele in data:
@@ -99,16 +123,28 @@ def index(request):
 
 			response_body = {
 				"question_text" : questions[i],
-				"type_LR_tfidf" : Type[0],
-				"probability_LR_tfidf" : probabilities[0],
-				"type_SVC_tfidf" : Type[1],
-				"probability_SVC_tfidf" : probabilities[1],
-				"type_NB_tfidf" : Type[2],
-				"probability_NB_count" : probabilities[2],
-				"type_LR_count" : Type[3],
-				"probability_LR_count" : probabilities[3],
-				"type_SVC_count" : Type[4],
-				"probability_SVC_count" : probabilities[4]
+				"models": {
+					"LR_TFIDF": {
+						"type": Type[0],
+						"probability": probabilities[0] 
+					},
+					"LF_COUNT": {
+						"type": Type[1],
+						"probability": probabilities[1] 
+					},
+					"SVC_TFIDF": {
+						"type": Type[2],
+						"probability": probabilities[2] 
+					},
+					"SVC_COUNT": {
+						"type": Type[3],
+						"probability": probabilities[3] 
+					},
+					"NB": {
+						"type": Type[4],
+						"probability": probabilities[4] 
+					}
+				}
 			}
 
 			# Type = ""
