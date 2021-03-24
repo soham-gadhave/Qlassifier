d89cf7961bc288ee569fea61b5823adcadea0f95
MohammedArshan <mohammedarshan.j@somaiya.edu>
2021-03-24 12:18:12 +0530

    post route

--- classifier/views.py
+++ classifier/views.py
@@ -82,85 +82,79 @@ def index(request):
 			
 			return response
 		except Exception as exception:
-			return JsonResponse({"message" : "Sorry something went wrong, counld not process ypur request. Try again", "error": exception}, status=500)
+			return JsonResponse({"message" : "Sorry something went wrong, counld not process your request. Try again", "error": exception}, status=500)
 
 	if request.method == "POST":
 
-		data = json.loads(request.body.decode("utf-8"))
-
+		try:
+			data = json.loads(request.body.decode("utf-8"))
+		except TypeError as ve:
+			JsonResponse({"message" : "Empty data. Try again", "error": ve}, status=204)
+		
 		print(data)
+		
+		try:
+			questions = []
+			try:
+				for ele in data:
+					questions.append(str(ele["question_text"]))
+			except KeyError as ke:
+				return JsonResponse({"message" : """question_text" attribute not found. Try again""", "error": ke}, status=300)
 
-		questions = []
-		for ele in data:
-			questions.append(str(ele["question_text"]))
+			x = clean_and_extract(questions)
+			x_te = x[0]
+			x_te1 = x[1]
+			x_c = x[2]
 
-		# X_te = clean_and_extract(questions)
+			prob_LR_tfidf = LR_tfidf.predict_proba(x_te)[:,1]
+			prob_SVC_tfidf = SVC_tfidf._predict_proba_lr(x_te)[:,1]
+			prob_NB_count = NB_count.predict_proba(x_c)[:,1]
+			prob_LR_count = LR_count.predict_proba(x_te1)[:,1]
+			prob_SVC_count = SVC_count._predict_proba_lr(x_te1)[:,1]
 
-		# probabilities = LR_tfidf[0].predict_proba(X_te)[:,1]
+			probabilities = [prob_LR_tfidf, prob_SVC_tfidf, prob_NB_count, prob_LR_count, prob_SVC_count]
+			Type = [[], [], [], [], []]
+			response = []
 
-		x = clean_and_extract(questions)
-		x_te = x[0]
-		x_te1 = x[1]
-		x_c = x[2]
+			for i in range(len(probabilities)):
+				for j in range(len(probabilities[i])):
+					if probabilities[i][j] >= 0.5:
+						Type[i].append("Insincere")
+					else:
+						Type[i].append("Sincere")
 
-		prob_LR_tfidf = LR_tfidf.predict_proba(x_te)[:,1][0]
-		prob_SVC_tfidf = SVC_tfidf._predict_proba_lr(x_te)[:,1][0]
-		prob_NB_count = NB_count.predict_proba(x_c)[:,1][0]
-		prob_LR_count = LR_count.predict_proba(x_te1)[:,1][0]
-		prob_SVC_count = SVC_count._predict_proba_lr(x_te1)[:,1][0]
-
-		probabilities = [prob_LR_tfidf, prob_SVC_tfidf, prob_NB_count, prob_LR_count, prob_SVC_count]	
-
-		response = []
-		for i in range(len(questions)):
-
-			Type = []
-			for prob in probabilities:
-				if prob >= 0.5:
-					Type.append("Insincere")
-				else:
-					Type.append("Sincere")
-
-			response_body = {
-				"question_text" : questions[i],
-				"models": {
-					"LR_TFIDF": {
-						"type": Type[0],
-						"probability": probabilities[0] 
-					},
-					"LF_COUNT": {
-						"type": Type[1],
-						"probability": probabilities[1] 
-					},
-					"SVC_TFIDF": {
-						"type": Type[2],
-						"probability": probabilities[2] 
-					},
-					"SVC_COUNT": {
-						"type": Type[3],
-						"probability": probabilities[3] 
-					},
-					"NB": {
-						"type": Type[4],
-						"probability": probabilities[4] 
+			for i in range(len(questions)):			
+				
+				response_body = {
+					"question_text" : questions[i],
+					"models": {
+						"LR_TFIDF": {
+							"type": Type[0][i],
+							"probability": probabilities[0][i] 
+						},
+						"LF_COUNT": {
+							"type": Type[1][i],
+							"probability": probabilities[1][i] 
+						},
+						"SVC_TFIDF": {
+							"type": Type[2][i],
+							"probability": probabilities[2][i] 
+						},
+						"SVC_COUNT": {
+							"type": Type[3][i],
+							"probability": probabilities[3][i]
+						},
+						"NB": {
+							"type": Type[4][i],
+							"probability": probabilities[4][i] 
+						}
 					}
 				}
-			}
 
-			# Type = ""
-			# if probabilities[i] >= 0.5:
-			# 	Type = "Insincere"
-			# else:
-			# 	Type = "Sincere"
+				response.append(response_body)
 
-			# response = {
-			# 	"question_text" : questions[i],
-			# 	"type" : Type,
-			# 	"probability" : float(probabilities[i]),
-			# 	"model" : "Logistic Regression"
-			# }
-			response.append(response_body)
+			response = JsonResponse(response, safe = False)
 
-		response = JsonResponse(response, safe = False)
-
-		return response
+			return response
+		except Exception as exception:
+			return JsonResponse({"message" : "Sorry something went wrong, counld not process your request. Try again", "error": exception}, status=500)
